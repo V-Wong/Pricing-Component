36cd21d7d3cbebd72850c374e5a567c87769278b
Vincent Wong <TheBrutux168@gmail.com>
2020-04-18 22:10:55 +1000

    Add title and slider to page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import PricingCardList from "./components/PricingCardList";
+import "./app.css"
 
 function App() {
   return (
-    <PricingCardList/>
+    <>
+      <div className="title">
+        <h1>Pricing Component Exercise</h1>
+      </div>
+      <div className="switch-container">
+        <p style={{marginRight: "1vw"}}>Monthly</p>
+        <label class="switch">
+          <input type="checkbox"/>
+          <span class="slider"></span>
+        </label>
+        <p style={{marginLeft: "1vw"}}>Annually</p>
+      </div>
+      <PricingCardList/>
+    </>
   );
 }
 
