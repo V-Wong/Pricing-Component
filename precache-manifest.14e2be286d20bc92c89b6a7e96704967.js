ee8b4ebfa3112b7569e7ca29989b4aba203cceb3
Vincent Wong <TheBrutux168@gmail.com>
2020-04-18 21:33:41 +1000

    Updates

---
+++ precache-manifest.14e2be286d20bc92c89b6a7e96704967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2de890e30267fbdb370113a9e7f2ec6",
+    "url": "/Pricing-Component/index.html"
+  },
+  {
+    "revision": "96b284ecfaf8f4b04c51",
+    "url": "/Pricing-Component/static/css/main.4ca2b8eb.chunk.css"
+  },
+  {
+    "revision": "7f5c2ff785468787316b",
+    "url": "/Pricing-Component/static/js/2.277244d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pricing-Component/static/js/2.277244d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b284ecfaf8f4b04c51",
+    "url": "/Pricing-Component/static/js/main.6e562117.chunk.js"
+  },
+  {
+    "revision": "a66567d750efdb7c2626",
+    "url": "/Pricing-Component/static/js/runtime-main.d571f556.js"
+  }
+]);
