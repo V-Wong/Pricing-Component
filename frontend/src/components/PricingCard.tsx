5568cb7bd85e989dbc6ea8a7d45fb832526109a9
Vincent Wong <TheBrutux168@gmail.com>
2020-04-18 13:38:35 +1000

    Add initial design for pricing card

---
+++ frontend/src/components/PricingCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./pricing-card.css";
+
+export default class PricingCard extends React.Component {
+  render() {
+    return (
+      <div className="pricing-card">
+        <p>Basic Plan</p>
+      </div>
+    )
+  }
+}
